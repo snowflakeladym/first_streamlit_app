f51485bdf92567234ad619a30cab8310aa2f0e82
Snowflake Lady <110149164+snowflakeladym@users.noreply.github.com>
2022-07-27 20:49:29 -0800

    Update streamlit_app.py
    added multi-select tool for fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom''s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -12,4 +12,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick some fruits:", list(myfruit_list.index)
+
 streamlit.dataframe(my_fruit_list)
